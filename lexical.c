0c0f095022b8ab9f354c9700ca3cbcbab1702702
Elhoover88 <pejcharstepangmail@.com>
2022-10-05 12:04:33 +0200

    adding string token state

--- lexical.c
+++ lexical.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 #include "lexical.h"
 #include "str.h"
 
@@ -11,14 +14,22 @@
 #define keywordOrIdentifierStateBegin 305 //the $ symbol tells us the next token will be a variable
 #define keywordOrIdentifierState 306 //reading the variable
 #define identifierOrKeywordState 307 //the next token will be an identifier or a keyword
-#define numberState 308 //the next token will be a number, we don't know the specific data type yet
+#define numberState 308 //the next token will be a number, we don't know the specific data type yet //todo numberstate
 #define smallerThanOrEqualState 309
 #define greaterThanOrEqualState 310
 #define assignOrEqualState 311
 #define equalState 312
-
-
-//todo token initialization, define returns of getNextToken function soMething like -> LEX_ERROR
+#define stringStartState 313
+#define waitForStringEnd 314
+#define variableRead 315
+#define backslashState 316
+#define escapeHexaState 317
+#define endHexaState 318
+#define escapeOctaState 319
+#define waitOctaState 320
+#define endOctaState 321
+#define numberState 322
+//todo token initialization, change returns of getNextToken function
 FILE *source;
 
 void setSourceFile(FILE *f){
@@ -30,7 +41,36 @@ void initToken(token *token){
     token->type = TYPE_INITIAL;
     strInit(token->content.str);
 }
+int octaToDecimal(const char *arr){
+    int octal = atoi(arr);
+    int rightmost;
+    int decimal;
+    int position;
 
+    while(octal > 0){
+        rightmost = octal / 10;
+        octal = octal / 10;
+
+        decimal = decimal + rightmost * pow(8, position);
+        position++;
+    }
+    return decimal;
+}
+int hexaToDecimal(const char arr[]){
+    int decimal = 0;
+    for(int i = 0; i< 2; i++){
+        if(arr[i] >= '0' && arr[i] <= '9'){
+            decimal += (arr[i] - 48) * 1;
+        }
+        else if(arr[i] >= 'A' && arr[i] <= 'F'){
+            decimal += (arr[i] -55) * 16;
+        }
+        else if(arr[i] >= 'a' && arr[i] <= 'f'){
+            decimal += (arr[i] -87) * 16;
+        }
+    }
+    return decimal;
+}
 /**
  * @brief function that checks if a string is a keyword and sets the token depending on the output
  * @param str dynamic string that we want to compare with a keyword
@@ -82,7 +122,7 @@ int keywordCmp(string *str, token *attr){
         attr->type = TYPE_IDENTIFIER;
         strCopyStr(attr->content.str, str);
     }
-    return 0;
+    return LEX_OK;
 
 };
 
@@ -95,6 +135,8 @@ int keywordCmp(string *str, token *attr){
 int getNextToken(token *attr) {
     int state = basicState;
     char character;
+    char hexaEscape[2];
+    char octaEscape[3];
     strInit(attr->content.str);
 
     strClean(attr->content.str);
@@ -115,7 +157,7 @@ int getNextToken(token *attr) {
                 }
                 else if(character == '$'){
                     strAddChar(attr->content.str, character);
-                    state = variableBeginState;
+                    state = variableRead;
                 }
                 else if(isalpha(character) || character == '_'){
                     state = identifierOrKeywordState;
@@ -153,9 +195,6 @@ int getNextToken(token *attr) {
                 else if(character == '='){
                     state = assignOrEqualState;
                 }
-
-
-
                 break;
                 /** if character is a comment **/
             case possibleCommentState :
@@ -207,9 +246,6 @@ int getNextToken(token *attr) {
                     strAddChar(attr->content.str, character);
                     state = keywordOrIdentifierState;
                 }
-                else if(character == EOF){
-                    return LEX_ERROR;
-                }
             case keywordOrIdentifierState:
                 if(isalnum(character) || character == '_'){
                     (char) tolower(character);
@@ -221,6 +257,119 @@ int getNextToken(token *attr) {
 
                 }
                 break;
+            case variableRead:
+                if(!(isspace(character))) {
+                    strAddChar(attr->content.str, character);
+                }
+                else{
+                    attr->type = TYPE_VARIABLE;
+                    ungetc(character, source);
+
+                }
+            case stringStartState:
+                if(character == '"'){
+                    state = waitForStringEnd;
+                }
+                else{
+                    return LEX_ERROR;
+                }
+                break;
+            case waitForStringEnd:
+                if(character > 31 && character != 92 ) {
+                    if (character == '"') {
+                        attr->type = TYPE_STRING;
+                        ungetc(character, source);
+                        return LEX_OK;
+                    }
+                    else {
+                        strAddChar(attr->content.str, character);
+
+                    }
+                }
+                else if(character == 92){
+                    state = backslashState;
+                }
+                    //backslash
+
+                else{
+                    return LEX_ERROR;
+                }
+            case backslashState:
+                if(character == 'x'){
+                    state = escapeHexaState;
+                }
+                else if(isdigit(character)){
+                    state = escapeOctaState;
+                }
+                else if(character == 't') {
+                    character = '\t';
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+                else if(character == 'n'){
+                    character = '\n';
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+                else {
+                    strAddChar(attr->content.str, 92);
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+            case escapeHexaState:
+                if(isxdigit(character)){
+                    strncat(hexaEscape, &character, 1);
+                    state = endHexaState;
+                }
+                else{
+                    strAddChar(attr->content.str, 92);
+                    strAddChar(attr->content.str, 'x');
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+            case endHexaState:
+                if(isxdigit(character)){
+                    strncat(hexaEscape, &character, 1);
+                    strAddChar(attr->content.str, (char) hexaToDecimal(hexaEscape));
+
+                }
+                else{
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+            case escapeOctaState:
+                if(isdigit(character)){
+                    strncat(octaEscape, &character, 1);
+                    state = waitOctaState;
+                }
+                else{
+                    strAddChar(attr->content.str, 92);
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+            case waitOctaState:
+                if(isdigit(character)){
+                    strncat(octaEscape, &character, 1);
+                    state = endOctaState;
+
+                }
+                else{
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+            case endOctaState:
+                if(isdigit(character)){
+                    strncat(octaEscape, &character, 1);
+                    strAddChar(attr->content.str, (char) octaToDecimal(octaEscape));
+                    state = waitForStringEnd;
+
+                }
+                else{
+                    strAddChar(attr->content.str, character);
+                    state = waitForStringEnd;
+                }
+
+
             case smallerThanOrEqualState:
                 if(character == '='){
                     attr->type = TYPE_SMALLER_OR_EQUAL;
