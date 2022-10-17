32ded0340cb9b9d694751aae5ea7e64228a1c5a8
Adam Nieslanik <adamnieslanik@Adams-MacBook-Air.local>
2022-10-17 17:21:02 +0200

    code_gen.c/h

---
+++ code_gen.h
@@ -0,0 +1,34 @@
+#ifndef CODE_GEN_H
+#define CODE_GEN_H
+
+
+#define MOVS 400
+#define ADDS 401
+#define SUBS 402
+#define MULS 403
+#define DIVS 404
+#define PUSHS 405
+#define POPS 406
+#define LTS 407
+#define GTS 408
+
+typedef struct instrList TInst;
+typedef struct instrNode TInstNode;
+
+
+typedef struct instrList
+{
+    TInst *first;
+    TInst *last; 
+};
+
+typedef struct instrNode
+{
+    TInstNode *param;
+    TInstNode *next;
+    int instruction;
+};
+
+
+
+#endif
